7cc4cc2123e46b63c861e115f86324347067f098
qc_f142df755deb4342b776331ccc96f8b1 <qc_f142df755deb4342b776331ccc96f8b1@git.code.tencent.com>
2025-01-03 00:43:31 +0800

    简单实现了一个识别口交和插入的方法

--- src/body_part.py
+++ src/body_part.py
@@ -1,3 +1,5 @@
+import dataclasses
+import functools
 import statistics
 import time
 import typing as t
@@ -7,36 +9,141 @@ import cv2
 import numpy as np
 from nudenet import NudeDetector
 
-from loader import iter_frame_bgr24
+from loader import iter_frame_bgr24, FrameRecord
 
 
-class _Record(t.NamedTuple):
-    score: float
-    img: np.ndarray | None
+def calculate_overlap_ratio(bbox1, bbox2) -> float:
+    # 解包bbox tuple (left, top, width, height)
+    left1, top1, w1, h1 = bbox1
+    left2, top2, w2, h2 = bbox2
 
-    def __bool__(self) -> bool:
-        return self.img is not None
+    # 计算每个bbox的边框
+    right1, bottom1 = left1 + w1, top1 + h1
+    right2, bottom2 = left2 + w2, top2 + h2
+
+    # 计算重叠区域的边框
+    overlap_left = max(left1, left2)
+    overlap_right = min(right1, right2)
+    overlap_top = max(top1, top2)
+    overlap_bottom = min(bottom1, bottom2)
+
+    # 检查是否有重叠
+    if overlap_left < overlap_right and overlap_top < overlap_bottom:
+        # 如果有重叠，计算重叠面积
+        overlap_width = overlap_right - overlap_left
+        overlap_height = overlap_bottom - overlap_top
+        overlap_area = overlap_width * overlap_height
+    else:
+        # 如果没有重叠，重叠面积为0
+        overlap_area = 0
+
+    # 计算两个bbox的面积
+    area1 = w1 * h1
+    area2 = w2 * h2
+
+    # 计算并集面积（总面积）
+    union_area = area1 + area2 - overlap_area
+
+    # 计算重叠比例
+    if union_area > 0:
+        overlap_ratio = overlap_area / union_area
+    else:
+        overlap_ratio = 0  # 防止除以零的情况
+
+    return overlap_ratio
+
+
+def calculate_enclosing_rectangle(bbox1, bbox2) -> tuple[float, float, float, float]:
+    # 解包bbox tuple (left, top, width, height)
+    left1, top1, w1, h1 = bbox1
+    left2, top2, w2, h2 = bbox2
+
+    # 计算每个bbox的边框
+    right1, bottom1 = left1 + w1, top1 + h1
+    right2, bottom2 = left2 + w2, top2 + h2
+
+    # 计算最小外接矩形的边框
+    enclosing_left = min(left1, left2)
+    enclosing_right = max(right1, right2)
+    enclosing_top = min(top1, top2)
+    enclosing_bottom = max(bottom1, bottom2)
+
+    # 返回最小外接矩形作为一个tuple (left, top, width, height)
+    return (
+        enclosing_left,
+        enclosing_top,
+        enclosing_right - enclosing_left,
+        enclosing_bottom - enclosing_top
+    )
+
+
+@dataclasses.dataclass
+class _Detection:
+    confidence: float = 0
+    bbox: t.Tuple[int, int, int, int] | None = None
+
+
+@dataclasses.dataclass
+class _RecordV2:
+    face: _Detection = dataclasses.field(default_factory=_Detection)
+    butt: _Detection = dataclasses.field(default_factory=_Detection)
+    breast: _Detection = dataclasses.field(default_factory=_Detection)
+    pussy: _Detection = dataclasses.field(default_factory=_Detection)
+    penis: _Detection = dataclasses.field(default_factory=_Detection)
+    feet: _Detection = dataclasses.field(default_factory=_Detection)
+
+    @staticmethod
+    def _create_overlap(dt_1: _Detection, dt_2: _Detection) -> _Detection:
+        pct = 0
+        bbox_target = None
+        if dt_1.confidence and dt_2.confidence:
+            overlap_ratio = calculate_overlap_ratio(dt_1.bbox, dt_2.bbox)
+            if overlap_ratio > 0:
+                pct = statistics.geometric_mean([dt_1.confidence, dt_2.confidence, overlap_ratio])
+                bbox_target = calculate_enclosing_rectangle(dt_1.bbox, dt_2.bbox)
+
+        return _Detection(pct, bbox_target)
+
+    @functools.cached_property
+    def oral_penetration(self) -> _Detection:
+        return self._create_overlap(self.face, self.penis)
+
+    @functools.cached_property
+    def vaginal_penetration(self) -> _Detection:
+        return self._create_overlap(self.pussy, self.penis)
 
 
 nude_detector = NudeDetector()
 
-target_class = [
-    "FACE_FEMALE",
-    "BUTTOCKS_EXPOSED",
-    "FEMALE_BREAST_EXPOSED",
-    "FEMALE_GENITALIA_EXPOSED",
-    "MALE_GENITALIA_EXPOSED",
-]
 
-p_for_test = 'D:\L5\[JAV] [Uncensored] FC2 PPV 1888207 [1080p]\FC2-PPV-1888207_1.mp4'
+def frame_image_to_detection(frame: np.ndarray) -> _RecordV2:
+    _record = _RecordV2()
+    detect_result_list = nude_detector.detect(frame)
+    for detect_result in detect_result_list:
+        detection = _Detection(detect_result['score'], tuple(detect_result['box']))
+        match detect_result['class']:
+            case 'FACE_FEMALE':
+                _record.face = detection
+            case 'BUTTOCKS_EXPOSED':
+                _record.butt = detection
+            case 'FEMALE_GENITALIA_EXPOSED':
+                _record.pussy = detection
+            case 'MALE_GENITALIA_EXPOSED':
+                _record.penis = detection
+            case 'FEMALE_BREAST_EXPOSED':
+                _record.breast = detection
+            case 'FEET_EXPOSED':
+                _record.feet = detection
+    return _record
 
 
-def detect_video_preview(video_path: Path) -> np.ndarray:
+def detect_video_preview(video_path: Path) -> list[tuple[FrameRecord, _RecordV2]]:
     detect_cost_list = []
     decode_cost_list = []
-    target_mapper = {k: _Record(0, None) for k in target_class}
+
+    res_list = []
     frame_generator = iter_frame_bgr24(video_path)
-    while not all(target_mapper.values()):
+    while True:
         try:
             decode_start_at = time.time()
             frame_record = next(frame_generator)
@@ -44,25 +151,22 @@ def detect_video_preview(video_path: Path) -> np.ndarray:
         except StopIteration:
             break
         detect_start_at = time.time()
-        detect_result_list = nude_detector.detect(frame_record.bgr_array)
+        detect_record = frame_image_to_detection(frame_record.bgr_array)
+        res_list.append((frame_record, detect_record))
         detect_cost_list.append(time.time() - detect_start_at)
-        for result in detect_result_list:
-            if result['class'] not in target_mapper:
-                continue
-            elif result['score'] < 0.75:
-                continue
-            else:
-                if target_mapper[result['class']].score < result['score']:
-                    target_mapper[result['class']] = _Record(result['score'], frame_record.bgr_array)
     print(f'{statistics.mean(detect_cost_list)=}, {statistics.stdev(detect_cost_list)=}, {len(detect_cost_list)=}')
     print(f'{statistics.mean(decode_cost_list)=}, {statistics.stdev(decode_cost_list)=},{len(decode_cost_list)=}')
-    to_stack_list = [r.img for r in target_mapper.values() if r.img is not None]
-    if to_stack_list:
-        return np.vstack(to_stack_list)
-    else:
-        return None
+    return res_list
 
 
 if __name__ == '__main__':
-    cv2.imshow('', cv2.resize(detect_video_preview(p_for_test), (640, 480)))
-    cv2.waitKey(0)
+    p_for_test = 'D:\L5\[JAV] [Uncensored] FC2 PPV 1888207 [1080p]\FC2-PPV-1888207_1.mp4'
+    result = detect_video_preview(p_for_test)
+    result_for_oral_sorted = sorted(result, key=lambda x: x[1].oral_penetration.confidence, reverse=True)
+    for result_for_oral in result_for_oral_sorted[0:5]:
+        cv2.imshow('', result_for_oral[0].bgr_array)
+        cv2.waitKey(0)
+    result_for_vaginal_sorted = sorted(result, key=lambda x: x[1].vaginal_penetration.confidence, reverse=True)
+    for result_for_vaginal in result_for_vaginal_sorted[0:5]:
+        cv2.imshow('', result_for_vaginal[0].bgr_array)
+        cv2.waitKey(0)
