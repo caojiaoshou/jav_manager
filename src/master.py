b4e985dae16b6773cc8f16c4dd663356be0333b3
qc_f142df755deb4342b776331ccc96f8b1 <qc_f142df755deb4342b776331ccc96f8b1@git.code.tencent.com>
2025-01-19 01:46:18 +0800

    修改master引入dao的方式.

--- src/master.py
+++ src/master.py
@@ -1,11 +1,9 @@
-import re
 import datetime
 import hashlib
 import re
 import time
 
-from src.dao import list_videos, add_video, delete_video, ProgressState, update_face, update_scene, update_body_part, \
-    update_quick_look, delete_quick_look, delete_body_part, delete_face, delete_scene, calculate_video_progress_state
+import src.dao as dao
 from src.file_index import search_local_videos
 from src.loader import get_video_duration
 from src.logger import configure_logger
@@ -39,7 +37,7 @@ def prepare_videos():
     current_local_mapper = {local_path.absolute(): dir_name for dir_name, local_path in search_local_videos()}
     _LOGGER.info(f'本地共 {len(current_local_mapper)} 个视频')
 
-    current_db_mapper = {r.file_path: r.id for r in list_videos()}
+    current_db_mapper = {r.file_path: r.id for r in dao.list_videos()}
     _LOGGER.info(f'数据库共 {len(current_db_mapper)} 个视频')
 
     for local_path, dir_name in current_local_mapper.items():
@@ -49,7 +47,7 @@ def prepare_videos():
 
             if is_video_valid:
                 video_hash = hashlib.md5(local_path.read_bytes()).hexdigest()
-                add_video(
+                dao.add_video(
                     _sanitize_name(dir_name),
                     local_path,
                     datetime.datetime.fromtimestamp(local_path.stat().st_ctime),
@@ -64,36 +62,36 @@ def prepare_videos():
 
     for local_path, id_ in current_db_mapper.items():
         if local_path not in current_local_mapper:
-            delete_video(id_)
+            dao.delete_video(id_)
             _LOGGER.info(f'从数据库删除了 {local_path}')
 
 
 def handle_views():
     prepare_videos()
-    db_videos = list_videos()
+    db_videos = dao.list_videos()
     _LOGGER.info(f'开始生成预览. 共 {len(db_videos)} 个视频')
-    for r in list_videos():
-        video_progress_state_value = calculate_video_progress_state(r)
+    for r in dao.list_videos():
+        video_progress_state_value = dao.calculate_video_progress_state(r)
         match video_progress_state_value:
-            case ProgressState.NOT_STARTED:
+            case dao.ProgressState.NOT_STARTED:
                 _LOGGER.info(f'开始处理 {r.file_path}')
                 start_at = time.time()
                 full_work_result = video_full_work(r.file_path)
                 _LOGGER.info(f'完成处理 {r.file_path} 用时 {time.time() - start_at:.2f}s')
-                update_quick_look(r.id, full_work_result.quick_look)
-                update_body_part(r.id, full_work_result.body_parts)
-                update_scene(r.id, full_work_result.scenes)
-                update_face(r.id, full_work_result.faces)
+                dao.update_quick_look(r.id, full_work_result.quick_look)
+                dao.update_body_part(r.id, full_work_result.body_parts)
+                dao.update_scene(r.id, full_work_result.scenes)
+                dao.update_face(r.id, full_work_result.faces)
                 _LOGGER.info(f'往数据库写入 {r.file_path} 的视频信息')
-            case ProgressState.IN_PROGRESS:
+            case dao.ProgressState.IN_PROGRESS:
                 _LOGGER.warning(f'开始删除 {r.file_path} 的视频信息.可能是由于此前错误退出')
                 start_at = time.time()
-                delete_quick_look(r.id)
-                delete_body_part(r.id)
-                delete_scene(r.id)
-                delete_face(r.id)
+                dao.delete_quick_look(r.id)
+                dao.delete_body_part(r.id)
+                dao.delete_scene(r.id)
+                dao.delete_face(r.id)
                 _LOGGER.warning(f'完成删除 {r.file_path} 的视频信息 用时 {time.time() - start_at:.2f}s')
-            case ProgressState.COMPLETED:
+            case dao.ProgressState.COMPLETED:
                 _LOGGER.info(f'忽略 {r.file_path} 此前处理完成')
                 continue
             case _:
@@ -102,7 +100,7 @@ def handle_views():
 
 
 def list_finish_views() -> list[id]:
-    return [r.id for r in list_videos() if calculate_video_progress_state(r) == ProgressState.COMPLETED]
+    return [r.id for r in dao.list_videos() if dao.calculate_video_progress_state(r) == dao.ProgressState.COMPLETED]
 
 
 if __name__ == '__main__':
