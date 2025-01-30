5e5470cd9e5dc9ac192c62e5c8d6dd5adf212005
qc_f142df755deb4342b776331ccc96f8b1 <qc_f142df755deb4342b776331ccc96f8b1@git.code.tencent.com>
2025-01-30 20:18:49 +0800

    抽离实现.

--- src/master.py
+++ src/master.py
@@ -1,7 +1,10 @@
 import datetime
+import functools
 import hashlib
 import re
+import threading
 import time
+import typing as t
 
 import src.dao as dao
 from src.audio_handler import audio_full_work
@@ -9,6 +12,18 @@ from src.file_index import search_local_videos
 from src.loader import get_video_duration
 from src.logger import configure_logger
 from src.video_handler import video_full_work
+
+_execution_lock: threading.RLock = threading.RLock()
+
+
+def _ensure_single_threading(func: t.Callable):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        with _execution_lock:
+            return func(*args, **kwargs)
+
+    return wrapper
+
 
 _LOGGER = configure_logger('master')
 
@@ -34,7 +49,8 @@ def _sanitize_name(name: str) -> str:
     return end if end else name
 
 
-def prepare_videos():
+@_ensure_single_threading
+def search_and_archive_video():
     current_local_mapper = {local_path.absolute(): dir_name for dir_name, local_path in search_local_videos()}
     _LOGGER.info(f'本地共 {len(current_local_mapper)} 个视频')
 
@@ -67,70 +83,78 @@ def prepare_videos():
             _LOGGER.info(f'从数据库删除了 {local_path}')
 
 
-def handle_views():
-    prepare_videos()
+@_ensure_single_threading
+def create_video_previews(video_record: dao.VideoInfo):
+    video_progress_state_value = dao.calculate_video_progress_state(video_record)
+    match video_progress_state_value:
+        case dao.ProgressState.NOT_STARTED:
+            _LOGGER.info(f'开始处理 {video_record.file_path}')
+            start_at = time.time()
+            full_work_result = video_full_work(video_record.file_path)
+            _LOGGER.info(f'完成处理 {video_record.file_path} 用时 {time.time() - start_at:.2f}s')
+            dao.update_quick_look(video_record.id, full_work_result.quick_look)
+            dao.update_body_part(video_record.id, full_work_result.body_parts)
+            dao.update_scene(video_record.id, full_work_result.scenes)
+            dao.update_face(video_record.id, full_work_result.faces)
+            _LOGGER.info(f'往数据库写入 {video_record.file_path} 的视频信息')
+        case dao.ProgressState.IN_PROGRESS:
+            _LOGGER.warning(f'开始删除 {video_record.file_path} 的视频信息.可能是由于此前错误退出')
+            start_at = time.time()
+            dao.delete_quick_look(video_record.id)
+            dao.delete_body_part(video_record.id)
+            dao.delete_scene(video_record.id)
+            dao.delete_face(video_record.id)
+            _LOGGER.warning(f'完成删除 {video_record.file_path} 的视频信息 用时 {time.time() - start_at:.2f}s')
+        case dao.ProgressState.COMPLETED:
+            _LOGGER.info(f'忽略 {video_record.file_path} 此前处理完成')
+        case _:
+            _LOGGER.error(f'未知的进度状态 {video_progress_state_value}')
+            raise ValueError(f'unknown progress state {video_progress_state_value}')
+
+
+def _handle_views_batched():
     db_videos = sorted(dao.list_videos(), key=lambda x: x.file_create_at, reverse=True)
     _LOGGER.info(f'开始生成预览. 共 {len(db_videos)} 个视频')
     for r in db_videos:
-        video_progress_state_value = dao.calculate_video_progress_state(r)
-        match video_progress_state_value:
-            case dao.ProgressState.NOT_STARTED:
-                _LOGGER.info(f'开始处理 {r.file_path}')
-                start_at = time.time()
-                full_work_result = video_full_work(r.file_path)
-                _LOGGER.info(f'完成处理 {r.file_path} 用时 {time.time() - start_at:.2f}s')
-                dao.update_quick_look(r.id, full_work_result.quick_look)
-                dao.update_body_part(r.id, full_work_result.body_parts)
-                dao.update_scene(r.id, full_work_result.scenes)
-                dao.update_face(r.id, full_work_result.faces)
-                _LOGGER.info(f'往数据库写入 {r.file_path} 的视频信息')
-            case dao.ProgressState.IN_PROGRESS:
-                _LOGGER.warning(f'开始删除 {r.file_path} 的视频信息.可能是由于此前错误退出')
-                start_at = time.time()
-                dao.delete_quick_look(r.id)
-                dao.delete_body_part(r.id)
-                dao.delete_scene(r.id)
-                dao.delete_face(r.id)
-                _LOGGER.warning(f'完成删除 {r.file_path} 的视频信息 用时 {time.time() - start_at:.2f}s')
-            case dao.ProgressState.COMPLETED:
-                _LOGGER.info(f'忽略 {r.file_path} 此前处理完成')
-                continue
-            case _:
-                _LOGGER.error(f'未知的进度状态 {video_progress_state_value}')
-                raise ValueError(f'unknown progress state {video_progress_state_value}')
+        create_video_previews(r)
 
 
-def handle_srt():
+@_ensure_single_threading
+def create_video_srt(video_ist: dao.VideoInfo):
+    audio_state = dao.calculate_audio_progress_state(video_ist)
+    match audio_state:
+        case dao.ProgressState.NOT_STARTED:
+            _LOGGER.info(f'开始处理 {video_ist.file_path} 的字幕')
+            start_at = time.time()
+            middle_ware_ls = audio_full_work(video_ist.file_path)
+            dao.update_srt(video_ist.id, middle_ware_ls)
+            _LOGGER.info(f'完成处理 {video_ist.file_path} 的字幕 用时 {time.time() - start_at:.2f}s')
+        case dao.ProgressState.IN_PROGRESS:
+            _LOGGER.warning(f'开始删除 {video_ist.file_path} 的字幕.可能是由于此前错误退出')
+            start_at = time.time()
+            dao.delete_srt(video_ist.id)
+            _LOGGER.warning(f'完成删除 {video_ist.file_path} 的字幕 用时 {time.time() - start_at:.2f}s')
+        case dao.ProgressState.COMPLETED:
+            _LOGGER.info(f'忽略 {video_ist.file_path} 此前处理完成')
+        case _:
+            _LOGGER.error(f'未知的进度状态 {audio_state}')
+            raise ValueError(f'unknown progress state {audio_state}')
+
+
+def _handle_srt_batched():
     for video_ist in sorted(dao.list_videos(), key=lambda x: x.file_create_at, reverse=True):
         video_state = dao.calculate_video_progress_state(video_ist)
         if video_state != dao.ProgressState.COMPLETED:
             continue
 
-        audio_state = dao.calculate_audio_progress_state(video_ist)
-        match audio_state:
-            case dao.ProgressState.NOT_STARTED:
-                _LOGGER.info(f'开始处理 {video_ist.file_path} 的字幕')
-                start_at = time.time()
-                middle_ware_ls = audio_full_work(video_ist.file_path)
-                dao.update_srt(video_ist.id, middle_ware_ls)
-                _LOGGER.info(f'完成处理 {video_ist.file_path} 的字幕 用时 {time.time() - start_at:.2f}s')
-            case dao.ProgressState.IN_PROGRESS:
-                _LOGGER.warning(f'开始删除 {video_ist.file_path} 的字幕.可能是由于此前错误退出')
-                start_at = time.time()
-                dao.delete_srt(video_ist.id)
-                _LOGGER.warning(f'完成删除 {video_ist.file_path} 的字幕 用时 {time.time() - start_at:.2f}s')
-            case dao.ProgressState.COMPLETED:
-                _LOGGER.info(f'忽略 {video_ist.file_path} 此前处理完成')
-                continue
-            case _:
-                _LOGGER.error(f'未知的进度状态 {audio_state}')
-                raise ValueError(f'unknown progress state {audio_state}')
+        create_video_srt(video_ist)
 
 
-def list_finish_views() -> list[id]:
-    return [r.id for r in dao.list_videos() if dao.calculate_video_progress_state(r) == dao.ProgressState.COMPLETED]
+def _module_direct_call():
+    search_and_archive_video()
+    _handle_views_batched()
+    _handle_srt_batched()
 
 
 if __name__ == '__main__':
-    handle_srt()
-    # handle_views()
+    _module_direct_call()
