fa3599ed5ca39e70a0a9145f544fef4f04a85eb5
qc_f142df755deb4342b776331ccc96f8b1 <qc_f142df755deb4342b776331ccc96f8b1@git.code.tencent.com>
2025-02-02 02:34:19 +0800

    添加一个单独识别人脸的方案

--- src/video_handler.py
+++ src/video_handler.py
@@ -37,7 +37,7 @@ class VideoFullWorkResult(t.NamedTuple):
     body_parts: list[VideoBodyPartParams]
 
 
-def video_full_work(p: pathlib.Path) -> VideoFullWorkResult:
+def _sample_keyframe(p: pathlib.Path) -> list[FrameRecord]:
     start_at = time.time()
     _LOGGER.debug(f'提取关键帧 {p}')
 
@@ -61,6 +61,33 @@ def video_full_work(p: pathlib.Path) -> VideoFullWorkResult:
                     to_pop_index = 0
     _LOGGER.debug(
         f'提取关键帧 用时 {time.time() - start_at:.2f}s, 找到 {original_key_frame_count} 帧,保留 {len(keyframe_record_list)} 帧')
+    return keyframe_record_list
+
+
+def _create_face_seq(p: pathlib.Path, prob_female_face_frames: list[np.ndarray]) -> list[VideoFaceParams]:
+    start_at = time.time()
+    face_seq = []
+    try:
+        best_female_face = select_best_female_face(prob_female_face_frames)
+        face_target_frame = prob_female_face_frames[best_female_face.frame_index]
+        face_crop_image = crop_and_rotate_face_into_square(
+            np.dstack([face_target_frame, get_foreground_mask(face_target_frame)]),
+            best_female_face.best
+        )
+        _LOGGER.debug(f'识别面部 用时 {time.time() - start_at:.2f}s')
+        face = VideoFaceParams(best_female_face.face_serial, best_female_face.age, face_target_frame, face_crop_image)
+        face_seq.append(face)
+    except FaceNotFoundError as e:
+        _LOGGER.debug(f'识别面部 用时 {time.time() - start_at:.2f}s')
+        _LOGGER.warning(f'未识别到女性面部 {p}, 原因:{e.args}')
+    except Exception as e:
+        _LOGGER.debug(f'识别面部 用时 {time.time() - start_at:.2f}s')
+        _LOGGER.warning(f'疑似内存不足错误 {p} {e}')
+    return face_seq
+
+
+def video_full_work(p: pathlib.Path) -> VideoFullWorkResult:
+    keyframe_record_list = _sample_keyframe(p)
 
     start_at = time.time()
     _LOGGER.debug(f'识别身体部位 {p}')
@@ -98,10 +125,9 @@ def video_full_work(p: pathlib.Path) -> VideoFullWorkResult:
     ]
 
     # 处理面部识别
-    start_at = time.time()
     _LOGGER.debug(f'识别面部 {p}')
     prob_female_face_frames = []
-    for i in np.linspace(0.7, 0.3, 8):
+    for i in np.linspace(0.6, 0.3, 8):
         prob_female_face_frames = [
             record_tuple[0].bgr_array
             for record_tuple in composite_list
@@ -109,23 +135,7 @@ def video_full_work(p: pathlib.Path) -> VideoFullWorkResult:
         ]
         if prob_female_face_frames:
             break
-    face_seq = []
-    try:
-        best_female_face = select_best_female_face(prob_female_face_frames)
-        face_target_frame = prob_female_face_frames[best_female_face.frame_index]
-        face_crop_image = crop_and_rotate_face_into_square(
-            np.dstack([face_target_frame, get_foreground_mask(face_target_frame)]),
-            best_female_face.best
-        )
-        _LOGGER.debug(f'识别面部 用时 {time.time() - start_at:.2f}s')
-        face = VideoFaceParams(best_female_face.face_serial, best_female_face.age, face_target_frame, face_crop_image)
-        face_seq.append(face)
-    except FaceNotFoundError as e:
-        _LOGGER.debug(f'识别面部 用时 {time.time() - start_at:.2f}s')
-        _LOGGER.warning(f'未识别到女性面部 {p}, 原因:{e.args}')
-    except Exception as e:
-        _LOGGER.debug(f'识别面部 用时 {time.time() - start_at:.2f}s')
-        _LOGGER.warning(f'疑似内存不足错误 {p} {e}')
+    face_seq = _create_face_seq(p, prob_female_face_frames)
     # 处理身体部位
     body_parts = []
     for part in ['butt', 'breast', 'pussy', 'feet', 'bar']:
@@ -164,7 +174,26 @@ def video_full_work(p: pathlib.Path) -> VideoFullWorkResult:
     return VideoFullWorkResult(face_seq, quick_look_video_bytes, scenes_ts_image, body_parts)
 
 
-if __name__ == '__main__':
+def face_work(p: pathlib.Path) -> list[VideoFaceParams]:
+    start_time = time.time()
+    key_frames = _sample_keyframe(p)
+    _LOGGER.debug(f'{p} io cost, {time.time() - start_time:.2f}')
+
+    start_time = time.time()
+    frames = [r.bgr_array for r in key_frames]
+    face_seq = _create_face_seq(p, frames)
+    _LOGGER.debug(f'{p} face cost, {time.time() - start_time:.2f}')
+    return face_seq
+
+
+def _test_face_only():
+    result = face_work(VIDEO_FILE_FOR_TEST)
+    _LOGGER.debug(f'{result[0].age, result[0].embedding}')
+    image_path = TEMP_STORAGE / VIDEO_FILE_FOR_TEST.with_suffix('.webp').name
+    cv2.imwrite(image_path.absolute().__str__(), result[0].crop_image)
+
+
+def _test_full_work():
     result = video_full_work(pathlib.Path(r'D:\L\SNIS-136\snis-136.mkv'))
 
     video_path = TEMP_STORAGE / VIDEO_FILE_FOR_TEST.with_suffix('.webm').name
@@ -178,3 +207,7 @@ if __name__ == '__main__':
 
     for body_record in result.body_parts:
         cv2.imwrite(TEMP_STORAGE / f'body_{body_record.part}.webp', body_record.frame)
+
+
+if __name__ == '__main__':
+    _test_face_only()
